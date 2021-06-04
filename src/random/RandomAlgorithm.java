468e0c1c454dd72adebf5ef76325b81e3e495291
Mário Cruz <2191262@my.ipleiria.pt>
2021-06-04 09:51:20 +0100

    Random

--- src/random/RandomAlgorithm.java
+++ src/random/RandomAlgorithm.java
@@ -1,6 +1,7 @@
 package random;
 
 import algorithms.Algorithm;
+import algorithms.AlgorithmEvent;
 import algorithms.Individual;
 import algorithms.Problem;
 
@@ -8,6 +9,13 @@ import java.util.Random;
 
 public class RandomAlgorithm<I extends Individual, P extends Problem<I>> extends Algorithm<I, P> {
     //TODO this class might require the definition of additional methods and/or attributes
+//    private  I individual;
+
+
+
+    // Tudo o que está feito foi basicamente copiado da outra ficha. A linha this.globalBest = individual.clone(); (utlima linha de todas) dá erro porque supostamente estamos a mandar
+    // individual em vez de I não percebo porque, mas tem a haver com a funcao clone. Basta fazer o cast que ele deixa de dar erro, se ta certo ou nao nao sei -> this.globalBest = (I) individual.clone();
+
 
     public RandomAlgorithm(int maxIterations, Random rand) {
         super(maxIterations, rand);
@@ -16,6 +24,28 @@ public class RandomAlgorithm<I extends Individual, P extends Problem<I>> extends
     @Override
     public I run(P problem) {
         //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+//        this.t = 0;
+//        this.individual = problem.getNewIndividual();
+//        this.individual.computeFitness();
+//        this.globalBest = this.individual;
+//        this.fireIterationEnded(new AlgorithmEvent(this));
+//
+//        while(this.t < this.maxIterations && !this.stopped) {
+//            this.individual = problem.getNewIndividual();
+//            this.individual.computeFitness();
+//            this.computeBestInRun(this.individual);
+//            ++this.t;
+//            this.fireIterationEnded(new AlgorithmEvent(this));
+//        }
+//
+//        this.fireRunEnded(new AlgorithmEvent(this));
+//        return this.globalBest;
+        return null;
     }
+
+//    private void computeBestInRun(I individual) {
+//        if (individual.compareTo(this.globalBest) > 0) {
+//            this.globalBest = (I) individual.clone();
+//        }
+//    }
 }
